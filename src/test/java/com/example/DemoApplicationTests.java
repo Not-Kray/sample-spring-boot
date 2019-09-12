540b62d1c852f47b9c06ab69e90e5940fe5e72c7
Not-Kray <sekou2005@hotmail.fr>
2019-09-12 12:02:52 +0200

    Ajout des tests et du plugin surefire

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,18 +1,21 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.Ignore;
-import static org.junit.Assert.assertTrue;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
 
 	@Test
-
 	public void myTest() {
 		assertTrue(true);
 	}
+
+}
