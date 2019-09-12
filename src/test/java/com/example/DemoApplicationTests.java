0e53cf7be3b77898de53d00a8e8842be9e24b3ff
Not-Kray <sekou2005@hotmail.fr>
2019-09-12 11:45:10 +0200

    ajout du test asserttrue

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,19 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	/*
-	*
-	*TEST JAVADOC
-	*
-	*/
-	
 	@Test
-	public void contextLoads() {
-	}
 
-}
+	public void myTest() {
+		assertTrue(true);
+	}
